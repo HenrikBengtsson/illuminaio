be9c7b242cc750a0cfbe4a5f19449e90554eba66
Kasper Daniel Hansen <kasperdanielhansen@gmail.com>
2012-11-13 00:54:44 +0000

    extended unitTests to also read a file from hapmap370k

--- inst/testData/testData.R
+++ inst/testData/testData.R
@@ -1,7 +1,13 @@
 library(illuminaio)
-library(minfiData)
 
+library(minfiData)
 idatDir <- system.file("extdata", "5723646052",  package = "minfiData")
 idatFile <- file.path(idatDir, "5723646052_R02C02_Grn.idat")
 idat.450k <- illuminaio::readIDAT(idatFile)
 save(idat.450k, file = "../unitTests/idat.450k.rda", compress = "xz")
+
+library(hapmap370k)
+idatDir <- system.file("idatFiles",  package = "hapmap370k")
+idatFile <- file.path(idatDir, "4019585376_B_Red.idat")
+idat.370k <- illuminaio::readIDAT(idatFile)
+save(idat.370k, file = "../unitTests/idat.370k.rda", compress = "xz")
--- inst/unitTests/test_readIDAT.R
+++ inst/unitTests/test_readIDAT.R
@@ -1,7 +1,15 @@
-test_readIDAT <- function() {
+test_readIDAT_minfiData<- function() {
     load(file.path(path.package("illuminaio"), "unitTests", "idat.450k.rda"))
     idatDir <- system.file("extdata", "5723646052",  package = "minfiData")
     idatFile <- file.path(idatDir, "5723646052_R02C02_Grn.idat")
     idat <- illuminaio::readIDAT(idatFile)
     checkEquals(idat, idat.450k)
+}
+
+test_readIDAT_hapmap370k<- function() {
+    load(file.path(path.package("illuminaio"), "unitTests", "idat.370k.rda"))
+    idatDir <- system.file("idatFiles",  package = "hapmap370k")
+    idatFile <- file.path(idatDir, "4019585376_B_Red.idat")
+    idat <- illuminaio::readIDAT(idatFile)
+    checkEquals(idat, idat.370k)
 }

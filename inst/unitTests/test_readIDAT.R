1818152a930382498badef9217b5116bae30e6e7
Kasper D. Hansen <khansen@stat.Berkeley.edu>
2013-07-04 22:03:36 +0000

    added vignette; changed dependencies to use new IlluminaDataTestFiles
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/illuminaio@78120 bc3139a8-67e5-0310-9ffc-ced21a209358

--- inst/unitTests/test_readIDAT.R
+++ inst/unitTests/test_readIDAT.R
@@ -1,15 +1,16 @@
-test_readIDAT_minfiData<- function() {
+test_readIDAT_minfiData <- function() {
     load(file.path(path.package("illuminaio"), "unitTests", "idat.450k.rda"))
-    idatDir <- system.file("extdata", "5723646052",  package = "minfiData")
-    idatFile <- file.path(idatDir, "5723646052_R02C02_Grn.idat")
+    idatFile <- system.file("extdata", "idat", "5723646052_R02C02_Grn.idat",
+                            package = "IlluminaDataTestFiles")
     idat <- illuminaio::readIDAT(idatFile)
     checkEquals(idat, idat.450k)
 }
 
-test_readIDAT_hapmap370k<- function() {
+test_readIDAT_hapmap370k <- function() {
     load(file.path(path.package("illuminaio"), "unitTests", "idat.370k.rda"))
-    idatDir <- system.file("idatFiles",  package = "hapmap370k")
-    idatFile <- file.path(idatDir, "4019585376_B_Red.idat")
+    idatFile <- system.file("extdata", "idat", "4019585376_B_Red.idat",
+                            package = "IlluminaDataTestFiles")
     idat <- illuminaio::readIDAT(idatFile)
     checkEquals(idat, idat.370k)
 }
+

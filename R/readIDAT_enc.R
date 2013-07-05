45bccae8259dcbd92a41862b97a8a8a03d767e17
Mike Smith <mls40@cam.ac.uk>
2013-07-05 15:01:37 +0000

    Improved performance reading certain encrypted IDAT files. Removed hardcoded limit on the number of beads to be read. Commented code and added more informative variable names.
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/illuminaio@78128 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/readIDAT_enc.R
+++ R/readIDAT_enc.R
@@ -19,59 +19,61 @@ readIDAT_enc<- function(file = NULL, verbose = FALSE) {
 
     what = c(rep("numeric", 6), rep("integer", 4))
     res <- list()
+    
+    tf <- tempfile(c("", ""));
 
     ## if we've got the XML format with lots of short lines we're
     ## going to find the lines that indicate the start of a new entry
     ## and then build each block of data by concatanating the lines inbetween 
     if(length(r) > 100) {
 
+        ## find the lines that contain the names for each block of data
         idx <- grep(" __", r)
-        idx <- c(idx, idx[length(idx)] + idx[2] - idx[1])
+        idx <- c(idx, grep(">", r)[2])
 
         for(i in 1:(length(idx)-1)) {
+                     
             tmp <- strsplit(r[idx[i]], " __")[[1]][2]
+
             name <- strsplit(tmp, "=\\\"")[[1]][1]
-            tmp <- strsplit(tmp, "=\\\"")[[1]][2]
 
-            for(j in (idx[i]+1):(idx[i+1]-1)) {
-                tmp <- paste(tmp, r[j], sep = "")
-            }
-            tmp <- paste(tmp, strsplit(r[idx[i+1]], "\ __")[[1]][1], sep = "")
-            if(i < length(idx)-1)
-                tmp <- substr(tmp, 1, nchar(tmp) - 1)
-            else
-                tmp <- substr(tmp, 1, nchar(tmp) - 2)
-
-            tf1 <- tempfile();
-            write.table(file = tf1, x = tmp, row.names = FALSE, col.names = FALSE, quote = FALSE)
-            tf2 <- tempfile();
-            decode(input = tf1, output = tf2)
-
-            res[[name]] <- readBin(tf2, what = what[i], size = 4, n = 50000L)
+            firstLine <- strsplit(tmp, "=\\\"")[[1]][2]
+            middleLines <- paste(r[ (idx[i]+1):(idx[i+1]-1) ], collapse = "")
+            finalLine <- strsplit(r[idx[i+1]], "\ __")[[1]][1]           
+            b64String <- paste(firstLine, middleLines, finalLine, sep = "")
             
-            file.remove(tf1, tf2)
+            ## remove quotation mark (and for last entry the closing angle bracket)
+            if(i < length(idx)-1)
+                b64String <- substr(b64String, 1, nchar(b64String) - 1)
+            else
+                b64String <- substr(b64String, 1, nchar(b64String) - 2)
+
+            ## write base64 to file, then decode, then write binary
+            write.table(file = tf[1], x = b64String, row.names = FALSE, col.names = FALSE, quote = FALSE)
+            decode(input = tf[1], output = tf[2])
+
+            ## estimated number of bead-types (plus a fudge factor to be sure)
+            nDataPoints <- (3 * nchar(b64String) / 16) + 100
+            res[[name]] <- readBin(tf[2], what = what[i], size = 4, n = as.integer(nDataPoints))           
         }
     } else {
         ## if we've got one long line for all the data chunks then we split it
         ## based on the position of the "__" indicating the data chunk names
         ## then process each of those in turn
-        if(verbose) 
-            message("Example Found ", file)
+       
         data <- strsplit(r[2], " __")[[1]]
         for(i in 2:length(data)) {
             tmp <- strsplit(data[[i]], "\\\"")[[1]]
             name <- substr(tmp[1], 1, nchar(tmp[1]) - 1)
-            tmp <- tmp[2]
+            b64String <- tmp[2]
 
-            tf1 <- tempfile();
-            write.table(file = tf1, x = tmp, row.names = FALSE, col.names = FALSE, quote = FALSE)
-            tf2 <- tempfile();
-            decode(input = tf1, output = tf2)
+            write.table(file = tf[1], x = b64String, row.names = FALSE, col.names = FALSE, quote = FALSE)
+            decode(input = tf[1], output = tf[2])
 
-            res[[name]] <- readBin(tf2, what = what[i-1], size = 4, n = 50000L)
-
-            file.remove(tf1, tf2);
+            nDataPoints <- (3 * nchar(b64String) / 16) + 100
+            res[[name]] <- readBin(tf[2], what = what[i-1], size = 4, n = as.integer(nDataPoints))
         }
     }
+    file.remove(tf[1], tf[2])
     return(as.data.frame(res))
 }

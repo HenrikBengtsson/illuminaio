ebe121ab6d095b5ab0dcd50574edc044211c7327
H Bengtsson <hb@aroma-project.org>
2013-07-30 05:38:33 +0000

    ROBUSTNESS/CLEANUP: Tidied up the code, e.g. using named arguments everywhere.
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/illuminaio@78955 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/readBPM.R
+++ R/readBPM.R
@@ -1,78 +1,100 @@
- readBPM <- function(file){
-
-     ## Reads and parses Illumina BPM files
-     
-     file <- path.expand(file)
-     tempCon <- file(file, "rb")
-
-     ## The first few bytes of the egtFile are some type of
-     ## header, but there's no related byte offset information.
-
-     prefixCheck <- readChar(tempCon,3) ## should be "BPM"
-
-     null.1 <- readBin(tempCon, "integer", n=1, size=1, signed=FALSE)
-     ## should be 1
-
-     versionNumber <-
-         readBin(tempCon, "integer", n=1, size=4, endian="little")
-     ## should be 4
-
-     nChars <- readBin(tempCon, "integer", n=1, size=1, signed=FALSE)
-     chipType <- readChar(tempCon, nChars)
-
-     null.2 <- readBin(tempCon, "integer", n=2, size=1, signed=FALSE)
-
-     csvLines <- readLines(tempCon, 22)
-
-     entriesByteOffset <- seek(tempCon);
-     nEntries <- readBin(tempCon, "integer", n=1, size=4,
-                         endian="little")
-
-     if(FALSE){
-
-         snpIndexByteOffset <- seek(tempCon)
-         snpIndex <- readBin(tempCon, "integer", n=nEntries, size=4,
-                             endian="little")
-         ## for the 1M array, these are simply in order from 1 to 1072820.
-
-         snpNamesByteOffset <- seek(tempCon)
-         snpNames <- rep("A", nEntries)
-         for(i1 in 1:nEntries){
-             nChars <- readBin(tempCon, "integer", n=1, size=1, signed=FALSE)
-             snpNames[i1] <- readChar(tempCon, nChars)
-         }
-
-     }
-
-     seek(tempCon, 15278138)
-
-     normIDByteOffset <- seek(tempCon)
-     normID <- readBin(tempCon, "integer", n=nEntries, size=1, signed=FALSE) + 1
-
-     newBlockByteOffset <- seek(tempCon)
-     newBlock <- readBin(tempCon, "integer", n=10000, size=1, signed=FALSE)
-
-     close(tempCon)
-
-     byteOffsets <- list(entriesByteOffset=entriesByteOffset,
-                         ##snpIndexByteOffset=snpIndexByteOffset,
-                         ##snpNamesByteOffset=snpNamesByteOffset,
-                         normIDByteOffset=normIDByteOffset,
-                         newBlockByteOffset=newBlockByteOffset)
-
-     allStuff <- list(prefixCheck=prefixCheck,
-                      null.1=null.1,
-                      versionNumber=versionNumber,
-                      chipType=chipType,
-                      null.2=null.2,
-                      csvLines=csvLines,
-                      nEntries=nEntries,
-                      ##snpIndex=snpIndex,
-                      ##snpNames=snpNames,
-                      normID=normID,
-                      newBlock=newBlock,
-                      byteOffsets=byteOffsets)
-     allStuff
- }
+readBPM <- function(file) {
+    ## - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+    ## Validate arguments
+    ## - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+    ## Argument 'file':
+    file <- path.expand(file)
+    if (!file_test("-f", file)) {
+        stop("File not found: ", file)
+    }
 
 
+    ## - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+    ## Open file (and make sure it's closed at the end)
+    ## - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+    con <- file(file, open="rb")
+    on.exit({
+        close(con)
+    })
+
+
+    ## - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+    ## Reads and parses Illumina BPM files
+    ## - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+    ## The first few bytes of the egtFile are some type of
+    ## header, but there's no related byte offset information.
+
+    ## Assert file format
+    prefixCheck <- readChar(con, nchars=3L) ## should be "BPM"
+##    if (prefixCheck != "BPM") {
+##        stop("Cannot read BPM file. File format error. Unknown magic: ", prefixCheck)
+##    }
+
+    null.1 <- readBin(con, what="integer", n=1L, size=1L, signed=FALSE)
+    ## should be 1
+
+    versionNumber <-
+        readBin(con, what="integer", n=1L, size=4L, endian="little")
+    ## should be 4
+##    if (versionNumber != 4L) {
+##        stop("Cannot read BPM file. Unsupported BPM file format version: ", versionNumber)
+##    }
+
+    nChars <- readBin(con, what="integer", n=1L, size=1L, signed=FALSE)
+    chipType <- readChar(con, nchars=nChars)
+
+    null.2 <- readBin(con, what="integer", n=2L, size=1L, signed=FALSE)
+
+    csvLines <- readLines(con, n=22L)
+
+    entriesByteOffset <- seek(con)
+    nEntries <- readBin(con, what="integer", n=1L, size=4L,
+                        endian="little")
+
+    if (FALSE) {
+        snpIndexByteOffset <- seek(con)
+        snpIndex <- readBin(con, what="integer", n=nEntries, size=4L,
+                            endian="little")
+        ## for the 1M array, these are simply in order from 1 to 1072820.
+
+        snpNamesByteOffset <- seek(con)
+        snpNames <- rep("A", times=nEntries)
+        for(i1 in 1:nEntries){
+            nChars <- readBin(con, what="integer", n=1L, size=1L, signed=FALSE)
+            snpNames[i1] <- readChar(con, nchars=nChars)
+        }
+    }
+
+    seek(con, where=15278138L)
+
+    normIDByteOffset <- seek(con)
+    normID <- readBin(con, what="integer", n=nEntries, size=1L, signed=FALSE) + 1L
+
+    newBlockByteOffset <- seek(con)
+    newBlock <- readBin(con, what="integer", n=10000L, size=1L, signed=FALSE)
+
+
+    ## - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+    ## Return results
+    ## - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+    byteOffsets <- list(entriesByteOffset=entriesByteOffset,
+                        ##snpIndexByteOffset=snpIndexByteOffset,
+                        ##snpNamesByteOffset=snpNamesByteOffset,
+                        normIDByteOffset=normIDByteOffset,
+                        newBlockByteOffset=newBlockByteOffset)
+
+    allStuff <- list(prefixCheck=prefixCheck,
+                     null.1=null.1,
+                     versionNumber=versionNumber,
+                     chipType=chipType,
+                     null.2=null.2,
+                     csvLines=csvLines,
+                     nEntries=nEntries,
+                     ##snpIndex=snpIndex,
+                     ##snpNames=snpNames,
+                     normID=normID,
+                     newBlock=newBlock,
+                     byteOffsets=byteOffsets)
+    allStuff
+} # readBPM()

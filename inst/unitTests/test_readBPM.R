c09d6ab1d429c420ad4a0adf306815ce7eb80316
H Bengtsson <hb@aroma-project.org>
2013-08-02 21:56:09 +0000

    Version: 0.3.5 [2013-08-02] o Cleaned up internal code of readBPM(). o ROBUSTNESS: Added unit tests for readBPM().  Note that these are   only run if environment variable '_R_CHECK_FULL_' is set, i.e.   they will *not* be perfomed on the Bioconductor servers.
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/illuminaio@79216 bc3139a8-67e5-0310-9ffc-ced21a209358

---
+++ inst/unitTests/test_readBPM.R
@@ -0,0 +1,48 @@
+## - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+## System tests for readBPM()
+##
+## Data is automatically downloaded from Illumina (e.g. [1]), if missing.
+##
+## NOTE: These tests will only run if '_R_CHECK_FULL_' is set, i.e.
+## they won't run on Bioconductor or CRAN servers.
+##
+## REFERENCES:
+## [1] http://support.illumina.com/downloads/infinium_bovineld_beadchip_product_files.ilmn
+## - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+## - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+## Utility functions
+## - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+getIlluminaAnnotationFile <- function(chipType, filename, force=FALSE) {
+  # Share data files across architectures, e.g. tests_i386 and tests_x64.
+  rootPath <- "../annotationData,illuminaio,tests"
+  path <- file.path(rootPath, "chipTypes", chipType)
+  if (!file_test("-d", path)) dir.create(path, recursive=TRUE)
+  pathname <- file.path(path, filename)
+
+  # Already downloaded?
+  if (!force && file_test("-f", pathname)) return(pathname)
+
+  # Download from Illumina
+  urlRoot <- "http://supportres.illumina.com/documents/downloads/productfiles"
+  url <- file.path(urlRoot, chipType, filename)
+  download.file(url, destfile=pathname, mode="wb")
+  if (!file_test("-f", pathname)) stop("Failed to download file: ", url)
+
+  pathname
+} # getIlluminaAnnotationFile()
+
+
+## - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+## Tests
+## - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+if (Sys.getenv("_R_CHECK_FULL_") != "") {
+
+test_readBPM_bovineld <- function() {
+  pathname <- getIlluminaAnnotationFile("bovineld", "bovineld_c.bpm")
+
+  data <- illuminaio::readBPM(pathname)
+  str(data)
+}
+
+} # if (Sys.getenv("_R_CHECK_FULL_") != "")

5c8622aa84a1140e1b1a5ca9099a8eb26b6a2551
Henrik Bengtsson <hb@aroma-project.org>
2022-09-23 07:38:14 -0700

    TESTS: Relax tests after now reading more 'Unknowns' fields

--- tests/readIDAT_450k.R
+++ tests/readIDAT_450k.R
@@ -4,4 +4,15 @@ load(file.path(path.package("IlluminaDataTestFiles"), "extdata", "testData", "id
 idatFile <- system.file("extdata", "idat", "5723646052_R02C02_Grn.idat",
                         package = "IlluminaDataTestFiles")
 idat <- illuminaio::readIDAT(idatFile)
+
+## WORKAROUND: Until 'IlluminaDataTestFiles' is updated, 
+## don't test all of the "Unknowns" fields
+if (packageVersion("illuminaio") >= "0.39.0-9004" &&
+    packageVersion("IlluminaDataTestFiles") <= "1.34.0") {
+  unknowns <- idat[["Unknowns"]]
+  unknowns[["Unknown.6"]] <- NULL
+  unknowns[["Unknown.7"]] <- NULL
+  idat[["Unknowns"]] <- unknowns
+}
+
 RUnit::checkEquals(idat, idat.450k)
